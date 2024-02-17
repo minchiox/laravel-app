ee8bc1b149a31c081e977439e747cf5dd808082f
minchiox <capuzzimatimichele06@gmail.com>
2024-02-17 15:12:51 +0100

    adding logo navbar

--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -8,13 +8,15 @@
 <body>
 <nav class="navbar navbar-light navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
     <div class="container">
-        <a class="navbar-brand mr-auto" href="#">Mexam</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
+                <li class="navbar-brand">
+                    <img  src="{{ asset('logo/Mexamlogo.png') }}" height='50'  alt="Logo">
+                </li>
                 @guest
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('login') }}">Login</a>
